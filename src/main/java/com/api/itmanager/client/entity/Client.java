131bdeb81654dadfbf11fafd163436ceb8807854
luisEdu <luisedu.unifal@gmail.com>
2021-11-13 14:50:19 -0300

    Projeto criado

---
+++ src/main/java/com/api/itmanager/client/entity/Client.java
@@ -0,0 +1,21 @@
+package com.api.itmanager.client.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String cnpj;
+
+    @Column(nullable = false)
+    private String address;
+
+}
