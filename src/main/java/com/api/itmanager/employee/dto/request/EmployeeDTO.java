190233215f45ad02cf30d22474e77ba33789d5b2
luisEdu <luisedu.unifal@gmail.com>
2021-11-21 15:18:06 -0300

    Fix in Employee Bean

--- src/main/java/com/api/itmanager/employee/dto/request/EmployeeDTO.java
+++ src/main/java/com/api/itmanager/employee/dto/request/EmployeeDTO.java
@@ -1,26 +1,40 @@
 package com.api.itmanager.employee.dto.request;
 
+import com.api.itmanager.client.entity.Client;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeDTO {
 
     private Long id;
+
+    @NotNull
+    private Client client;
 
     @NotEmpty
     @Size(min = 5, max = 50)
     private String name;
 
     @NotEmpty
-    @Size(min = 10, max = 10)
+    @Size(min = 10, max = 12)
     private String admissionDate;
 
-    @Size(min = 10, max = 10)
+    @Size(min = 10, max = 12)
     private String integrationDate;
 
-    @Size(min = 10, max = 10)
+    @Size(min = 10, max = 12)
     private String resignationDate;
 
-    @Size(min = 10, max = 11)
+    @Size(min = 10, max = 12)
     private String mainPhoneNumber;
 }
