a0a4fa81d520948270746786b90d458e7a0a58ce
luisEdu <luisedu.unifal@gmail.com>
2021-12-11 09:30:28 -0300

    Swagger POST codes return

--- src/main/java/com/api/itmanager/config/SwaggerConfig.java
+++ src/main/java/com/api/itmanager/config/SwaggerConfig.java
@@ -2,11 +2,18 @@ package com.api.itmanager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ResponseMessage;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -17,6 +24,24 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.POST, responseMessageForPOST());
+    }
+
+    private List<ResponseMessage> responseMessageForPOST()
+    {
+        return new ArrayList<ResponseMessage>() {{
+            add(new ResponseMessageBuilder()
+                    .code(500)
+                    .message("Internal Server Error - Unique Fields")
+                    .responseModel(new ModelRef("Internal Server Error"))
+                    .build());
+            add(new ResponseMessageBuilder()
+                    .code(400)
+                    .message("Bad Request - Rules for not null and min/max")
+                    .responseModel(new ModelRef("Bad Request"))
+                    .build());
+        }};
     }
 }
