c4f42b65f5549702c2b99365c0e159f1e17577d5
luisEdu <luisedu.unifal@gmail.com>
2022-03-12 16:02:41 -0300

    Device Entity

---
+++ src/main/java/com/api/itmanager/modules/infrastructure/device/model/Device.java
@@ -0,0 +1,49 @@
+package com.api.itmanager.modules.infrastructure.device.model;
+
+import com.api.itmanager.modules.client.model.Client;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "DEVICE")
+public class Device {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "FK_CLIENT", nullable = false)
+    private Client clientID;
+
+    @Column(name = "CATEGORY", nullable = false)
+    @Size(min = 2, max = 20)
+    private String category;
+
+    @Column(name = "BRAND", nullable = false)
+    @Size(min = 3, max = 20)
+    private String brand;
+
+    @Column(name = "MODEL", nullable = false)
+    @Size(min = 3, max = 20)
+    private String model;
+
+    @Column(name = "MAC_ADDRESS", length = 12)
+    private String macAddress;
+
+    @Column(name = "IP_ADDRESS", length = 12)
+    private String ipAddress;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+}
